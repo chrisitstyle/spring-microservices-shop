1551214c0f06548fbd29b686c21de3ca28a685c3
chrisitstyle <43932836+chrisitstyle@users.noreply.github.com>
2026-08-16 22:09:09 +0200

    refactor: extract user service

---
+++ user-service/src/main/java/pl/chrisitstyle/user/exception/GlobalExceptionHandler.java
@@ -0,0 +1,82 @@
+package pl.chrisitstyle.user.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.Instant;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<ErrorDTO> handleUserNotFound(
+            UserNotFoundException exception
+    ) {
+        return createErrorResponse(
+                exception.getMessage(),
+                HttpStatus.NOT_FOUND
+        );
+    }
+
+    @ExceptionHandler(UserAlreadyExistsException.class)
+    public ResponseEntity<ErrorDTO> handleUserAlreadyExists(
+            UserAlreadyExistsException exception
+    ) {
+        return createErrorResponse(
+                exception.getMessage(),
+                HttpStatus.CONFLICT
+        );
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorDTO> handleValidation(
+            MethodArgumentNotValidException exception
+    ) {
+        Map<String, String> fieldErrors = new LinkedHashMap<>();
+
+        exception.getBindingResult()
+                .getFieldErrors()
+                .forEach(error ->
+                        fieldErrors.putIfAbsent(
+                                error.getField(),
+                                error.getDefaultMessage()
+                        )
+                );
+
+        return createErrorResponse(
+                "Validation failed",
+                HttpStatus.BAD_REQUEST,
+                fieldErrors
+        );
+    }
+
+    private ResponseEntity<ErrorDTO> createErrorResponse(
+            String message,
+            HttpStatus status) {
+        return createErrorResponse(
+                message,
+                status,
+                Map.of());
+    }
+
+    private ResponseEntity<ErrorDTO> createErrorResponse(
+            String message,
+            HttpStatus status,
+            Map<String, String> fieldErrors
+    ) {
+        ErrorDTO errorDTO = new ErrorDTO(
+                status.value(),
+                status.name(),
+                message,
+                Instant.now(),
+                fieldErrors);
+
+        return new ResponseEntity<>(errorDTO, status);
+    }
+}
