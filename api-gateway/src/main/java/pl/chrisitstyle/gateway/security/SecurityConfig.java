311549fc0c69bfa0f181ad7cb29e94fb6a283652
chrisitstyle <43932836+chrisitstyle@users.noreply.github.com>
2026-08-22 14:05:26 +0200

    feat: add secured API gateway

---
+++ api-gateway/src/main/java/pl/chrisitstyle/gateway/security/SecurityConfig.java
@@ -0,0 +1,71 @@
+package pl.chrisitstyle.gateway.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(
+            HttpSecurity http
+    ) throws Exception {
+
+        JwtAuthenticationConverter jwtAuthenticationConverter =
+                new JwtAuthenticationConverter();
+
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(
+                new KeycloakRealmRolesConverter()
+        );
+
+        jwtAuthenticationConverter.setPrincipalClaimName(
+                "preferred_username"
+        );
+
+        return http
+                .csrf(csrf -> csrf.disable())
+
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(
+                                SessionCreationPolicy.STATELESS
+                        )
+                )
+
+                .authorizeHttpRequests(authorize ->
+                        authorize
+                                .requestMatchers("/actuator/**")
+                                .permitAll()
+
+                                .requestMatchers(
+                                        HttpMethod.GET,
+                                        "/products",
+                                        "/products/**"
+                                )
+                                .permitAll()
+
+                                .requestMatchers(
+                                        "/orders",
+                                        "/orders/**"
+                                )
+                                .authenticated()
+
+                                .anyRequest()
+                                .denyAll()
+                )
+
+                .oauth2ResourceServer(oauth2 ->
+                        oauth2.jwt(jwt ->
+                                jwt.jwtAuthenticationConverter(
+                                        jwtAuthenticationConverter
+                                )
+                        )
+                )
+
+                .build();
+    }
+}
