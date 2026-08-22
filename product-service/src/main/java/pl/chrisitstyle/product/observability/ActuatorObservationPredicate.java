48760d1fc517d95f9c7fb562d48a6786ada8c05b
chrisitstyle <43932836+chrisitstyle@users.noreply.github.com>
2026-08-22 21:43:13 +0200

    refactor: reduce tracing noise from actuator and security

---
+++ product-service/src/main/java/pl/chrisitstyle/product/observability/ActuatorObservationPredicate.java
@@ -0,0 +1,22 @@
+package pl.chrisitstyle.product.observability;
+
+import io.micrometer.observation.Observation;
+import io.micrometer.observation.ObservationPredicate;
+import org.springframework.http.server.observation.ServerRequestObservationContext;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ActuatorObservationPredicate implements ObservationPredicate {
+
+    private static final String ACTUATOR_PREFIX = "/actuator";
+
+    @Override
+    public boolean test(String name, Observation.Context context) {
+        if (context instanceof ServerRequestObservationContext serverContext) {
+            String requestUri = serverContext.getCarrier().getRequestURI();
+            return !requestUri.startsWith(ACTUATOR_PREFIX);
+        }
+
+        return true;
+    }
+}
